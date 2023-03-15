480f698868b1d1369a79cd963056ea62ee1d8ce2
Aryan Parashar <91550780+aryanparashar23@users.noreply.github.com>
2023-03-15 22:39:40 +0530

    Add files via upload

---
+++ Untitled-1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+ using namespace std;
+
+int main (){
+    int f;
+    cout << "Enter the Fah value" << endl;
+    cin >> f;
+    int c = (5/9) * (f - 32 );
+    cout << c <<endl;
+    
+
+}
+
